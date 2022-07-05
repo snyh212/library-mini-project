678933829348acc34637cf4d6ca8355d784e24cd
ericrothmuller <eric@skymarketingco.com>
2022-07-05 15:29:27 -0700

    Added form ID to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,7 @@
+searchForm = getElementById("searchform");
 
-var searchField = getElementById("input");
+function formSubmit(event) {
+    event.preventDefault();
 
-var searchTerm = searchField.value.trim();
-
-var outputField = getElementById("content");
-
-
-
-displaySearch = function (data) {
-    outputField.textContent = "data";
-};
-
-
-var getSearchResults = function (searchTerm) {
-    var searchUrl = "https://www.loc.gov/search/?q=" + searchTerm + "&fo=json";
-    // var searchUrl = "https://www.loc.gov/search/?q=baseball&fo=json"; THIS WAS USED AS AN EXAMPLE
-
-    fetch(searchUrl).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-                displaySearch (data);
-            }); 
-        } else {
-            alert("Error: No Data To Display");
-        }
-    }
-    
-    )
+    var searchInput = 
 }
-
-getSearchResults();
-
---
+++ assets/search-results.js
@@ -0,0 +1,35 @@
+
+var searchField = getElementById("input");
+
+var searchTerm = searchField.value.trim();
+
+var outputField = getElementById("content");
+
+
+
+
+displaySearch = function (data) {
+    outputField.textContent = "data";
+};
+
+
+var getSearchResults = function (searchTerm) {
+    var searchUrl = "https://www.loc.gov/search/?q=" + searchTerm + "&fo=json";
+    // var searchUrl = "https://www.loc.gov/search/?q=baseball&fo=json"; THIS WAS USED AS AN EXAMPLE
+
+    fetch(searchUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+                displaySearch (data);
+            }); 
+        } else {
+            alert("Error: No Data To Display");
+        }
+    }
+    
+    )
+}
+
+getSearchResults();
+
