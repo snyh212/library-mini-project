e77b26653f60e1c2de6a1968c649b9c28b8e0a4c
ericrothmuller <eric@skymarketingco.com>
2022-07-05 15:54:04 -0700

    Added JS functionality for the search button

--- assets/script.js
+++ assets/script.js
@@ -3,5 +3,18 @@ searchForm = getElementById("searchform");
 function formSubmit(event) {
     event.preventDefault();
 
-    // var searchInput = 
+    var searchInput = getElementById("input").value;
+    var formatInput = getElementById("dropDown").value;
+
+    if (!searchInput) {
+        alert("You need an input value!");
+        return;
+    }
+
 }
+
+var searchUrl = "./search-results.html?q=" + searchInput + '&format=' + formatInputVal;
+
+localStorage.assign(searchUrl);
+
+searchForm.addEventListener("submit", formSubmit);
